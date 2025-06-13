fe23bafb573ef0e23cfdd1bbbe45fc72af686aa7
arakiztain <arakiztain.gaizka@gmail.com>
2025-06-13 21:27:28 +0200

    index model

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,21 +1,33 @@
-import sequelize from "../config/sequelize.js";
-import User from "./user.js";
-import Group from "./group.js";
-import Municipality from "./municipality.js";
-import GroupMember from "./groupMember.js";
+import { Sequelize } from 'sequelize';
+import UserModel from './user.js';
+import MunicipalityModel from './municipality.js';
+import UserMunicipalityModel from './userMunicipality.js';
+import GroupModel from './group.js';
+import GroupMemberModel from './groupMember.js';
 
-// Relación usuarios <-> grupos (a través de group_members)
-User.belongsToMany(Group, { through: GroupMember });
-Group.belongsToMany(User, { through: GroupMember });
+const sequelize = new Sequelize(process.env.DB_URL); // o config específico
 
-// Relación usuario <-> municipios
-User.hasMany(Municipality);
-Municipality.belongsTo(User);
+const User = UserModel(sequelize);
+const Municipality = MunicipalityModel(sequelize);
+const UserMunicipality = UserMunicipalityModel(sequelize);
+const Group = GroupModel(sequelize);
+const GroupMember = GroupMemberModel(sequelize);
+
+// Relaciones
+
+// Usuarios - Municipios (Many-to-Many) via UserMunicipality
+User.belongsToMany(Municipality, { through: UserMunicipality, foreignKey: 'userId' });
+Municipality.belongsToMany(User, { through: UserMunicipality, foreignKey: 'municipalityId' });
+
+// Grupos - Usuarios (Many-to-Many) via GroupMember
+Group.belongsToMany(User, { through: GroupMember, foreignKey: 'groupId' });
+User.belongsToMany(Group, { through: GroupMember, foreignKey: 'userId' });
 
 export {
   sequelize,
   User,
-  Group,
   Municipality,
-  GroupMember,
+  UserMunicipality,
+  Group,
+  GroupMember
 };
