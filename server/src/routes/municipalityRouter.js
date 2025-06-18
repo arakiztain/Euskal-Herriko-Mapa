9e017fc07f618f0c7c515e0e667e1c9a1c540cf6
arakiztain <arakiztain.gaizka@gmail.com>
2025-06-18 21:27:34 +0200

    Add municipalities and paint

--- server/src/routes/municipalityRouter.js
+++ server/src/routes/municipalityRouter.js
@@ -6,6 +6,7 @@ const router = Router();
 router.get("/", municipalityController.getAllMunicipalities);
 router.get("/user", municipalityController.getUserMunicipalities);
 router.post("/", municipalityController.addUserMunicipality);
+router.get("/search", municipalityController.searchMunicipalities);
 router.delete("/:municipalityId", municipalityController.removeUserMunicipality);
 
 
